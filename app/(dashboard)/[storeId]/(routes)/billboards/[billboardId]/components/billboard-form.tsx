1f9687ec59f09f41a2077be697a7a01ba0f635e2
nono <nono.polkadot@gmail.com>
2023-10-31 10:51:13 +0800

    feat(billboard): fix main nav & delete unuse code

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -15,7 +15,6 @@ import toast from "react-hot-toast"
 import { useParams, useRouter } from "next/navigation"
 import axios from "axios"
 import { AlertModal } from "@/components/modals/alert-modal"
-import { ApiAlert } from "@/components/ui/api-alert"
 import { useOrigin } from "@/hooks/use.origin"
 import ImageUpload from "@/components/ui/image-upload"
 
@@ -35,7 +34,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 }) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
