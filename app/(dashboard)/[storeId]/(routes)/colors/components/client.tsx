138549fe21de55d9293f407aff2d8bf2ebccacc4
nono <nono.polkadot@gmail.com>
2023-11-01 14:28:48 +0800

    create colors feature

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { ColorColoum, columns } from "./coloumns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
+
+interface ColorClientProps {
+  data: ColorColoum[]
+}
+
+export const ColorClient: React.FC<ColorClientProps> = ({
+  data
+}) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="manage size for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading 
+        title="API"
+        description="Api Calls for Colors"
+      />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  )
+}
